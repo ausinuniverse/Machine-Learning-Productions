52e43c2d2b5fa0082a2f2e7b4215802c2146f38a
Utkarsh <utkarshsinha1000@gmail.com>
2024-12-31 13:14:50 +0530

    C

---
+++ variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+ int password = 100;
+ float password_2 = 1.999;
+ float password_3 = 2.111;
+ printf("%d\n",password);
+ printf("%.4f\n",password_2);
+ printf("%f\n",password_3);
+ return 0;
+}
